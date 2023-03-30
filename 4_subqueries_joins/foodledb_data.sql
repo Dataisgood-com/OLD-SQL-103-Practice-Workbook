fbc8abd2e6a983b87e7ce492124ce028b33b5859
Ankit Maheshwari <maheshwari.ankit@gmail.com>
2023-03-30 11:14:30 +0530

    Update foodledb_data.sql

--- 4_subqueries_joins/foodledb_data.sql
+++ 4_subqueries_joins/foodledb_data.sql
@@ -1,1 +1,73 @@
 
+-- Populate the users table
+INSERT INTO users (name, email, password, phone_number, delivery_address)
+VALUES
+('Rashmika Mandanna', 'rashmika@example.com', 'password', '9876543210', 'Bengaluru'),
+('Himesh Reshammiya', 'himesh@example.com', 'password', '9876543210', 'Mumbai'),
+('Katrina Kaif', 'katrina@example.com', 'password', '9876543210', 'Mumbai'),
+('Samantha Ruth Prabhu', 'samantha@example.com', 'password', '9876543210', 'Chennai'),
+('Virat Kohli', 'virat@example.com', 'password', '9876543210', 'Delhi'),
+('Rohit Sharma', 'rohit@example.com', 'password', '9876543210', 'Mumbai'),
+('MS Dhoni', 'msdhoni@example.com', 'password', '9876543210', 'Ranchi'),
+('Alia Bhatt', 'alia@example.com', 'password', '9876543210', 'Mumbai'),
+('Shah Rukh Khan', 'srk@example.com', 'password', '9876543210', 'Mumbai'),
+('Deepika Padukone', 'deepika@example.com', 'password', '9876543210', 'Bengaluru'),
+('Ranveer Singh', 'ranveer@example.com', 'password', '9876543210', 'Mumbai'),
+('Anushka Sharma', 'anushka@example.com', 'password', '9876543210', 'Mumbai'),
+('Kareena Kapoor', 'kareena@example.com', 'password', '9876543210', 'Mumbai'),
+('Saif Ali Khan', 'saif@example.com', 'password', '9876543210', 'Mumbai'),
+('Ajay Devgn', 'ajay@example.com', 'password', '9876543210', 'Mumbai'),
+('Akshay Kumar', 'akshay@example.com', 'password', '9876543210', 'Mumbai'),
+('Kangana Ranaut', 'kangana@example.com', 'password', '9876543210', 'Mumbai'),
+('Amitabh Bachchan', 'amitabh@example.com', 'password', '9876543210', 'Mumbai'),
+('Salman Khan', 'salman@example.com', 'password', '9876543210', 'Mumbai'),
+('Aamir Khan', 'aamir@example.com', 'password', '9876543210', 'Mumbai'),
+('Rajinikanth', 'rajinikanth@example.com', 'password', '9876543210', 'Chennai'),
+('Vijay', 'vijay@example.com', 'password', '9876543210', 'Chennai'),
+('Mahesh Babu', 'mahesh@example.com', 'password', '9876543210', 'Hyderabad'),
+('Allu Arjun', 'allu@example.com', 'password', '9876543210', 'Hyderabad'),
+('Prabhas', 'prabhas@example.com', 'password', '9876543210', 'Hyderabad'),
+('Nayanthara', 'nayanthara@example.com', 'password', '9876543210', 'Chennai');
+
+
+-- Populate the restaurants table
+INSERT INTO restaurants (name, address, phone_number, cuisine, rating)
+VALUES
+('Pind Balluchi', '2nd Floor, Building No.8, Cyber City, Gurgaon', '0124456789', 'North Indian', 4.1),
+('Oh Calcutta', 'P-48, Biren Roy Rd W, Behala, Kolkata', '0334466778', 'Bengali', 4.2),
+('Carnatic Cafe', 'No. 22, 2nd Main Rd, Vyalikaval, Bengaluru', '0805566778', 'South Indian', 4.6),
+('Taj Mahal Tea House', '36/A, Sanjana Plaza, Chapel Rd, Bandra West, Mumbai', '0223366554', 'Tea', 4.4),
+('Maharaja Bhog', '2nd Floor, Phoenix Market City, LBS Road, Kurla West, Mumbai', '0222345678', 'Gujarati', 4.5),
+('Paradise Biryani', 'D Block, 6-3-870, Greenlands Rd, Begumpet, Hyderabad', '0402222777', 'Biryani', 3.7),
+('Tunday Kababi', '168/6, Birhana Rd, Kaiserbagh, Lucknow', '0522333444', 'Awadhi', 4.1),
+('Peter Cat', '18A, Park St, Kolkata', '0334455888', 'Continental', 4.3),
+('Karim's', '16, Gali Kababian, Jama Masjid, Old Delhi', '0112345678', 'Mughlai', 3.6),
+('Bikanerwala', 'Ground Floor, MGF Metropolitan Mall, Saket, New Delhi', '0112345678', 'North Indian', 4.2),
+('Saravana Bhavan', 'No 32, Ground Floor, Evershine Mall, Link Road, Malad West, Mumbai', '0222288666', 'South Indian', 4.4),
+('Nizam's', 'Near Charminar, Hyderabad', '0402233444', 'Hyderabadi', 3.5),
+('Peshawari', 'ITC Maratha, Sahar Road, Opposite Chhatrapati Shivaji Airport, Andheri East, Mumbai', '0222233444', 'North Indian', 4.7),
+('Karavalli', 'Gateway Hotel, 66, Residency Rd, Shanthala Nagar, Ashok Nagar, Bengaluru', '0802233444', 'Coastal', 4.1),
+('Trishna', '7, Saibaba Marg, Kala Ghoda, Fort, Mumbai', '0222266160', 'Seafood', 4.3),
+('Bhagat Tarachand', 'North Avenue Road, Santacruz West, Mumbai', '0222345678', 'Gujarati', 4.6),
+('Moti Mahal Delux', '10/48, Kasturba Gandhi Marg, Connaught Place, New Delhi', '0112332444', 'Mughlai', 4.2),
+('Kesar da Dhaba', 'Chowk Passian, Near Telephone Exchange, Amritsar', '0183222777', 'Punjabi', 2.4),
+('Khyber', '145, Mahatma Gandhi Road, Kala Ghoda, Fort, Mumbai', '0222267322', 'North Indian', 4.5),
+('The Bombay Canteen', 'Kamala Mills, Lower Parel, Mumbai', '02249666666', 'Indian', 4.8),
+('SodaBottleOpenerWala', 'Ground Floor, The Capital, G Block, Bandra Kurla Complex, Bandra East, Mumbai', '02265555595', 'Parsi, Iranian, Indian', 4.3),
+('Oh! Calcutta', '10, Juhu Tara Road, Juhu, Mumbai', '02266158765', 'Bengali, Seafood', 4.0),
+('Kappa Chakka Kandhari', 'Link Road, Oshiwara, Andheri West, Mumbai', '02240031200', 'South Indian', 4.2),
+('Dum Pukht', 'ITC Maratha, Sahar Airport Road, Andheri East, Mumbai', '02228303030', 'North Indian', 3.5),
+('Gajalee', 'Hanuman Road, Vile Parle East, Mumbai', '02226141901', 'Seafood, Maharashtrian', 4.1),
+('Café Madras', 'King's Circle, Matunga East, Mumbai', '02224143128', 'South Indian', 4.0),
+('Bastian', 'B/1, New Kamal Building, Linking Road, Bandra West, Mumbai', '02226425544', 'Seafood, European, American', 4.6),
+('Yauatcha', 'Raheja Tower, Bandra Kurla Complex, Bandra East, Mumbai', '09222222800', 'Chinese, Dim Sum', 4.4),
+('Hauz Khas Social', '12, Hauz Khas Village, New Delhi', '07838652823', 'Continental, American, North Indian', 4.3),
+('The Big Chill Café', '68-A, Khan Market, New Delhi', '01141757588', 'Continental, Italian, Desserts', 4.5),
+('Saravana Bhavan', 'P-13, Connaught Circus, Connaught Place, New Delhi', '01123417910', 'South Indian', 4.2),
+('Bukhara', 'ITC Maurya, Chanakyapuri, New Delhi', '01126112233', 'North Indian, Mughlai', 4.7),
+('Dum Pukht', 'ITC Maurya, Chanakyapuri, New Delhi', '01126112233', 'North Indian', 4.6),
+('Karim's', '16, Gali Kababian, Jama Masjid, New Delhi', '01123269880', 'Mughlai, North Indian', 4.0),
+('Rajinder Da Dhaba', 'AB 14, Safdarjung Enclave Market, Safdarjung, New Delhi', '01126100359', 'North Indian', 4.1),
+('The China Kitchen', 'Hyatt Regency, Bhikaji Cama Place, New Delhi', '01166771334', 'Chinese', 4.4);
+
+
